5527802fac87b2170bd1a42dad472f5ba77a3868
horacioud <20121346@ce.pucmm.edu.do>
2017-04-15 19:58:28 -0400

    Algoritmo de Comparacion de IMG's Terminado

--- CompararIMG/src/compararimg/CompararIMG.java
+++ CompararIMG/src/compararimg/CompararIMG.java
@@ -1,5 +1,7 @@
 package compararimg;
 
+import java.io.FileReader;
+
 /**
  *
  * @author Horacio Ureña
@@ -7,7 +9,31 @@ package compararimg;
 public class CompararIMG {
 
     public static void main(String[] args) {
-        
+        try {
+            boolean found = false;
+            String image1 = "gymDog.jpg";
+            String image2 = "gymDog2.jpg";
+            FileReader fr1 = new FileReader ( image1 );
+            FileReader fr2 = new FileReader ( image2 );
+            while ( true ){
+                int pix1 = fr1.read ( );
+                int pix2 = fr2.read ( );
+                if ( pix1 != pix2 ){
+                    break;
+                }
+                // pix1 y pix2 son  iguales...
+                // si una de las dos es -1 se acabo el fichero
+                if ( pix1 == -1 ){
+                    found = true;
+                    break;
+                }
+            }
+            if ( found ) System.out.println ( "Imagenes iguales" );
+            else System.out.println ( "Imagenes diferentes" );
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
     
 }
